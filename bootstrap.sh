71227d7b3fa6d8c072e4a806e8cf111299f49307
Nicolas Sosa <nico77@gmail.com>
2019-10-08 13:11:19 -0300

    Agregadas las configs de vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Create links for dotfiles
 
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
