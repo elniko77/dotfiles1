f8b801441717297275911a0cc31e5fea8e492202
nss <nss@ares.uces.edu.ar>
2021-06-10 11:16:42 -0300

    install oh my zsh

---
+++ install_ohmy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install zsh
+chsh -s /bin/zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
